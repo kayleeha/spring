714857c7e8103b00fe0b1dd2905679907335e2a7
kayleeha <ampmgrow.go@gmail.com>
2022-12-03 21:36:10 +0900

    commit

---
+++ src/spring/di/Program.java
@@ -0,0 +1,14 @@
+package spring.di;
+
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Exam exam = new NewlecExam(); // NewlecExam은 구현 객체 Exam은 인터페이스로 부모
+
+	}
+
+}
